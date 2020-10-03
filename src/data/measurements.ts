99f29110deb2cb70d9940199fa0cbaddc5060179
Pete O'Shea <mail@peteoshea.co.uk>
2020-10-03 14:04:13 +0100

    Replace the sample data with a couple of example weights

--- src/data/measurements.ts
+++ src/data/measurements.ts
@@ -1,58 +1,19 @@
 export interface Measurement {
-  fromName: string;
-  subject: string;
+  weight: string;
   date: string;
   id: number;
 }
 
 const measurements: Measurement[] = [
   {
-    fromName: 'Matt Chorsey',
-    subject: 'New event: Trip to Vegas',
-    date: '9:32 AM',
+    weight: '70',
+    date: '10 aug',
     id: 0,
   },
   {
-    fromName: 'Lauren Ruthford',
-    subject: 'Long time no chat',
-    date: '6:12 AM',
+    weight: '71',
+    date: '16 aug',
     id: 1,
-  },
-  {
-    fromName: 'Jordan Firth',
-    subject: 'Report Results',
-    date: '4:55 AM',
-    id: 2,
-  },
-  {
-    fromName: 'Bill Thomas',
-    subject: 'The situation',
-    date: 'Yesterday',
-    id: 3,
-  },
-  {
-    fromName: 'Joanne Pollan',
-    subject: 'Updated invitation: Swim lessons',
-    date: 'Yesterday',
-    id: 4,
-  },
-  {
-    fromName: 'Andrea Cornerston',
-    subject: 'Last minute ask',
-    date: 'Yesterday',
-    id: 5,
-  },
-  {
-    fromName: 'Moe Chamont',
-    subject: 'Family Calendar - Version 1',
-    date: 'Last Week',
-    id: 6,
-  },
-  {
-    fromName: 'Kelly Richardson',
-    subject: 'Placeholder Headhots',
-    date: 'Last Week',
-    id: 7,
   },
 ];
 
--- src/pages/ViewMeasurement.tsx
+++ src/pages/ViewMeasurement.tsx
@@ -44,7 +44,7 @@ const ViewMeasurement: React.FC<ViewMeasurementProps> = ({ match }) => {
               <IonIcon icon={personCircle} color="primary"></IonIcon>
               <IonLabel className="ion-text-wrap">
                 <h2>
-                  {measurement.fromName}
+                  {measurement.weight}
                   <span className="date">
                     <IonNote>{measurement.date}</IonNote>
                   </span>
@@ -54,18 +54,6 @@ const ViewMeasurement: React.FC<ViewMeasurementProps> = ({ match }) => {
                 </h3>
               </IonLabel>
             </IonItem>
-
-            <div className="ion-padding">
-              <h1>{measurement.subject}</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-              </p>
-            </div>
           </>
         ) : (
           <div>Measurement not found</div>
